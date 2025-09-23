9c1d76b77dddd94946b7b2b9206f7c41030ac8bb
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-09-24 00:15:38 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ def not_found(err):
     return "нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
-                <a href= "/author">author</a>   
+                <a href= "/lab1/author">author</a>   
             </body>
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Чинкевич Кирилл Алексеевич"
     group = "ФБИ-32"
@@ -32,11 +32,11 @@ def author():
                <p>Студент: """ + name + """</p>
                <p>Группа: """ + group + """</p>
                <p>Факультет: """ + faculty + """</p>
-               <a href= "/web">web</a>
+               <a href= "/lab1/web">web</a>
             </body>
         </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image():
     path = url_for("static", filename="oak.jpg")
     css = url_for("static", filename="lab1.css")
@@ -55,13 +55,13 @@ def image():
 
 count = 0
 
-@app.route('/counter/clear')
+@app.route('/lab1/counter/clear')
 def clear_counter():
     global count
     count = 0
-    return redirect('/counter')
+    return redirect('/lab1/counter')
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -84,9 +84,9 @@ def counter():
 </html>
 '''
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route('/created')
 def created():
