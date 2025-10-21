8f6fed1323ffae74bf6f2f3cb1234d2a849a340f
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-10-21 23:32:14 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 import datetime
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 
 # Добавляем глобальную переменную для хранения лога в начало файла (после импортов)
@@ -118,6 +120,7 @@ def index():
             <ul>
                 <li><a href="/lab1">Лабораторная работа 1</a></li>
                 <li><a href="/lab2">Лабораторная работа 2</a></li>
+                <li><a href="/lab3">Лабораторная работа 3</a></li>
             </ul>
         </div>
         <footer>
