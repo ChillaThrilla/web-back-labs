8a6c0fe8bfbc365af5b2e2ced4bd7eb95e058147
Чинкевич Кирилл <mellow_beef@mail.ru>
2025-10-22 18:43:04 +0700

    заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 import datetime
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 # Добавляем глобальную переменную для хранения лога в начало файла (после импортов)
