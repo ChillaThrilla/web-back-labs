93e470dfacb19201f690488552789dbf6c3a7c15
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-11-04 20:28:38 +0700

    Деление

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
+
 import datetime
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 # Добавляем глобальную переменную для хранения лога в начало файла (после импортов)
@@ -121,6 +124,7 @@ def index():
                 <li><a href="/lab1">Лабораторная работа 1</a></li>
                 <li><a href="/lab2">Лабораторная работа 2</a></li>
                 <li><a href="/lab3">Лабораторная работа 3</a></li>
+                <li><a href="/lab4">Лабораторная работа 4</a></li>
             </ul>
         </div>
         <footer>
