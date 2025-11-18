046296d2dea2d0c9772505316b298d8cd5e9f7e1
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-11-18 23:14:15 +0700

    Правки для SQLite 4

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 import os
 from dotenv import load_dotenv
+load_dotenv()
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 
-
-load_dotenv()
 
 import datetime
 app = Flask(__name__)
