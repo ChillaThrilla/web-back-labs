b78f17214807831f8517564f0d682b4487877827
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-12-08 23:20:04 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 
 
@@ -19,6 +20,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 
 
@@ -142,6 +144,7 @@ def index():
                 <li><a href="/lab5">Лабораторная работа 5</a></li>
                 <li><a href="/lab6">Лабораторная работа 6</a></li>
                 <li><a href="/lab7">Лабораторная работа 7</a></li>
+                <li><a href="/lab8">Лабораторная работа 8</a></li>
             </ul>
         </div>
         <footer>
