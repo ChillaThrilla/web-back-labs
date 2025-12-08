a66b203fe6345128edcfa9a274e97f473159d5a7
Кирилл Чинкевич <mellow_beef@mail.ru>
2025-12-08 20:04:47 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -23,6 +23,9 @@ function fillFilmList() {
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function () {
+                editFilm(i);
+            };
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -88,6 +91,7 @@ function addFilm() {
 
 
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
@@ -95,8 +99,8 @@ function sendFilm() {
         description: document.getElementById('description').value
     };
 
-    const url = '/lab7/rest-api/films/';
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
 
     fetch(url, {
         method: method,
@@ -109,3 +113,21 @@ function sendFilm() {
     });
 }
 
+
+
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (film) {
+
+            document.getElementById('id').value = id;
+            document.getElementById('title').value = film.title;
+            document.getElementById('title-ru').value = film.title_ru;
+            document.getElementById('year').value = film.year;
+            document.getElementById('description').value = film.description;
+
+            showModal();
+        });
+}
